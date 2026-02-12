5b6323aacf9be51bacfa96c7d3ccb8c33c631b50
annabellemclean1 <amclean2019@icloud.com>
2026-02-12 15:07:20 -0500

    Adding images back after OAuth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { supabase } from '@/lib/supabase';
+import { supabase } from '../lib/supabase';
 import { User, AuthChangeEvent, Session } from '@supabase/supabase-js';
 
 export default function Home() {
